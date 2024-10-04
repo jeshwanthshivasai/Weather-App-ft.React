4f6eaf8b7840043a77be26dfaa4c3ea62b65e613
jeshwanthshivasai <git@stackblitz.com>
2024-10-04 11:35:23 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+  let api = {
+    key: '820b817bc39d52dac15c774cb4cd0fe1',
+    url: 'https://api.openweathermap.org/data/2.5/weather',
+  };
+
+  return (
+    <div>
+      <input type="text" />
+      <button>Search</button>
+    </div>
+  );
+};
